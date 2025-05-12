7a1abde7d8937544839c25344ceab941fac676fa
rickrayol <rickrayol.3@gmail.com>
2025-05-11 22:56:43 -0300

    Ajustes para o Deploy

--- back-end/API/connect.js
+++ back-end/API/connect.js
@@ -1,9 +1,9 @@
 import { MongoClient } from "mongodb";
 
-const URL =
+const URI =
   "mongodb+srv://rickrayol:heartVol3@cluster0.qxwfxrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const client = new MongoClient(URL);
+const client = new MongoClient(URI);
 
 export const db = client.db("SpotifyProjeto");
 // const songCollection = await db.collection("songs").find({}).toArray();
--- back-end/API/server.js
+++ back-end/API/server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3018;
 
-// Middleware, colocar sempre antes da requisição e escuta, senão não vai funcionar.
 app.use(cors());
 
-app.get("/", (request, response) => {
-  response.send("Olá, mundo! Salva, nóis");
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("/api/", (request, response) => {
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
+
 app.listen(PORT, () => {});
+console.log(`O servidor está escutando na porta ${PORT}`);
